d28ebae26b6770f786b7de28b55ca34f920aa08e
Mayara Rysia <mayara.ryzia@gmail.com>
2018-02-16 16:12:42 -0200

    exercícios do capítulo 09

---
+++ eloquente/capitulo09/exercicio01_regexp_golf.js
@@ -0,0 +1,41 @@
+// Fill in the regular expressions
+
+verify(/(car|cat)/,
+       ["my car", "bad cats"],
+       ["camper", "high art"]);
+
+verify(/(po|pr)/,
+	   ["pop culture", "mad props"], ["plop"]);
+
+verify(/ferr(et|y|ari)/,
+	   ["ferret", "ferry", "ferrari"],
+	   ["ferrum", "transfer A"]);
+
+verify(/ious\b/,
+	   ["how delicious", "spacious room"],
+	   ["ruinous", "consciousness"]);
+
+verify(/\s[.,:;]/,
+	   ["bad punctuation ."],
+	   ["escape the dot"]);
+
+verify(/\w{7,}/,
+	   ["hottentottententen"],
+	   ["no", "hotten totten tenten"]);
+
+verify(/\b[^\we]+\b/,
+	   ["red platypus", "wobbling nest"],
+	   ["earth bed", "learning ape"]);
+
+function verify(regexp, yes, no) {
+  // Ignore unfinished tests
+  if (regexp.source == "...") return;
+  yes.forEach(function(s) {
+	if (!regexp.test(s))
+	  console.log("Failure to match '" + s + "'");
+  });
+  no.forEach(function(s) {
+	if (!regexp.test(s))
+	  console.log("Unexpected match for '" + s + "'");
+  });
+}
